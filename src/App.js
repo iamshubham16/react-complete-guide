3b50f7dbded3bab2f6e37f0285884516c9c992ca
iamshubham16 <guglanishubham16@gmail.com>
2020-03-18 06:23:25 +0530

    Removed style components from app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import styled from 'styled-components';
 import Person from './Person/Person';
 
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};;
-    color: black;
-`;
 
 class App extends Component {
   state = {
@@ -84,11 +72,6 @@ class App extends Component {
           })}
         </div>
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color:'black'
-      // }
     } 
     
     const classes = [];
@@ -102,10 +85,9 @@ class App extends Component {
         <div className = "App">
           <h1>Hello, My name is Shubham</h1>
           <p className={classes.join(' ')}>Is this working???</p>
-          <StyledButton
-            alt = {this.state.showPersons}  
+          <button
             onClick={this.togglePersonHandler}>Toggle Person
-          </StyledButton>
+          </button>
             {persons}              
         </div>
     );
