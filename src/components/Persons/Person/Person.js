cfa134a7607bf878af9310e33c98ccbbf7424821
iamshubham16 <guglanishubham16@gmail.com>
2020-03-19 05:28:16 +0530

    Adding component lifecycle-creation

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,6 +3,7 @@ import classes from  './Person.module.css'
 
 
 const person = (props) => {
+    console.log('[person.js] rendering')
     return (
         <div className = {classes.Person}> 
             <p onClick = {props.click}> I am {props.name} and I am {props.age} yerars old </p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+  
   state = {
     persons : [
       {id : '1', name : 'Shubham', age : '22'},
@@ -11,6 +17,15 @@ class App extends Component {
       {id : '3', name : 'Shrishti', age : '22'}
     ],
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] component did mount');
   }
 
   switchNameHandler = (newName) => {
@@ -58,6 +73,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
     let persons = null;
     if(this.state.showPersons) {
       persons = (
